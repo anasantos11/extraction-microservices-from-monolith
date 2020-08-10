4d94654efd6d9c41d756fb55580e467dfaee68da
Ana Santos <anasantos1103@gmail.com>
2020-08-10 01:49:47 -0300

    Created getters in BaseMethodDTO class.

--- src/main/java/br/com/pucminas/dtos/BaseMethodDTO.java
+++ src/main/java/br/com/pucminas/dtos/BaseMethodDTO.java
@@ -3,9 +3,8 @@ package br.com.pucminas.dtos;
 public class BaseMethodDTO {
 	private String className;
 	private String methodName;
-	
-	public BaseMethodDTO(String className, String methodName)
-	{
+
+	public BaseMethodDTO(String className, String methodName) {
 		this.className = className;
 		this.methodName = methodName;
 	}
@@ -17,10 +16,17 @@ public class BaseMethodDTO {
 	public String getMethodName() {
 		return methodName;
 	}
-	
+
+	public String getSourceFileName() {
+		return className.replace('.', '/') + ".java";
+	}
+
+	public String getFullName() {
+		return this.className + ":" + this.methodName;
+	}
+
 	@Override
-	public String toString()
-	{
+	public String toString() {
 		return "Class Name = " + this.className + "; Method Name = " + this.methodName;
 	}
 }
