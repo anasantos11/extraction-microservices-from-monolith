5057a24774277e02fadb56aa02fe6bb00255ba1f
Ana Santos <anasantos1103@gmail.com>
2020-08-20 01:39:24 -0300

    Created method to group services in microservices.

--- src/main/java/br/com/pucminas/businesslogics/ServiceBL.java
+++ src/main/java/br/com/pucminas/businesslogics/ServiceBL.java
@@ -2,14 +2,16 @@ package br.com.pucminas.businesslogics;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.eclipse.jgit.api.errors.GitAPIException;
 
 import br.com.pucminas.domain.ClassName;
 import br.com.pucminas.domain.Method;
-import br.com.pucminas.domain.Similarity;
+import br.com.pucminas.domain.SimilarityMatrixCell;
 import br.com.pucminas.dtos.CommitDTO;
 import br.com.pucminas.dtos.ParentMethodDTO;
 import br.com.pucminas.repositories.CallGraphRepository;
@@ -31,6 +33,49 @@ public class ServiceBL {
 		this.weightMethodItem = weightMethodItem;
 		this.weightHistoryItem = weightHistoryItem;
 		this.lowerLimitToGroup = lowerLimitToGroup;
+	}
+
+	public Map<String, List<Method>> groupServicesInMicroservices() throws IOException, GitAPIException {
+		SimilarityMatrixCell[][] similarityMatrix = generateSimilarityMatrix();
+		Map<String, List<Method>> microservices = new HashMap<>();
+		int numberOfMicroservices = 0;
+
+		for (int r = 0; r < similarityMatrix.length; r++) {
+			List<Method> similarServices = new ArrayList<>();
+			similarServices.add(similarityMatrix[r][0].getLineMethod());
+
+			for (int c = 0; c < similarityMatrix.length; c++) {
+				SimilarityMatrixCell similarity = similarityMatrix[r][c];
+
+				if (similarity.getSimilarity() >= lowerLimitToGroup)
+					similarServices.add(similarity.getLineMethod());
+			}
+
+			if (anyMicroserviceContainsAnySimilarService(similarServices, microservices)) {
+				addSimilarServicesInExistsMicroservice(similarServices, microservices);
+			} else {
+				microservices.put("M" + numberOfMicroservices, similarServices);
+				++numberOfMicroservices;
+			}
+		}
+
+		return microservices;
+	}
+
+	private boolean anyMicroserviceContainsAnySimilarService(List<Method> similarServices,
+			Map<String, List<Method>> microservices) {
+		return similarServices.stream().anyMatch(
+				service -> microservices.values().stream().anyMatch(microservice -> microservice.contains(service)));
+	}
+
+	private void addSimilarServicesInExistsMicroservice(List<Method> similarServices,
+			Map<String, List<Method>> microservices) {
+		for (List<Method> services : microservices.values()) {
+			if (similarServices.stream().anyMatch(service -> services.contains(service))) {
+				services.addAll(similarServices);
+				return;
+			}
+		}
 	}
 
 	public List<Method> getCandidateServices() throws IOException, GitAPIException {
@@ -64,21 +109,22 @@ public class ServiceBL {
 		return methods;
 	}
 
-	public Similarity[][] generateSimilarityMatrix() throws IOException, GitAPIException {
+	private SimilarityMatrixCell[][] generateSimilarityMatrix() throws IOException, GitAPIException {
 		List<Method> candidateServices = getCandidateServices().stream()
 				.filter(candidateService -> candidateService.isCandidateService()).collect(Collectors.toList());
 
-		Similarity[][] similarityMatrix = new Similarity[candidateServices.size()][candidateServices.size()];
+		SimilarityMatrixCell[][] similarityMatrix = new SimilarityMatrixCell[candidateServices.size()][candidateServices
+				.size()];
 		int rowIndex = 0;
 		int columnIndex = 0;
 
 		for (Method lineMethod : candidateServices) {
 			for (Method columnMethod : candidateServices) {
-				similarityMatrix[rowIndex][columnIndex] = new Similarity(lineMethod, columnMethod,
+				similarityMatrix[rowIndex][columnIndex] = new SimilarityMatrixCell(lineMethod, columnMethod,
 						calculateSimilarity(lineMethod, columnMethod));
 			}
 		}
-		
+
 		return similarityMatrix;
 	}
 
@@ -92,11 +138,11 @@ public class ServiceBL {
 		long numberMethods = a.getMethods().size();
 		long numberCommitIds = a.getCommitIds().size();
 
-		double similarity = (numberEqualClasses * this.weightClassItem + numberEqualMethods * this.weightMethodItem
-				+ numberEqualCommitIds * this.weightHistoryItem)
-				/ (numberClasses * this.weightClassItem + numberMethods * this.weightMethodItem
-						+ numberCommitIds * this.weightHistoryItem);
+		long numerator = numberEqualClasses * this.weightClassItem + numberEqualMethods * this.weightMethodItem
+				+ numberEqualCommitIds * this.weightHistoryItem;
+		long denominator = numberClasses * this.weightClassItem + numberMethods * this.weightMethodItem
+				+ numberCommitIds * this.weightHistoryItem;
 
-		return similarity * 100;
+		return denominator > 0 ? numerator / denominator * 100 : 0;
 	}
 }
--- src/main/java/br/com/pucminas/domain/SimilarityMatrixCell.java
+++ src/main/java/br/com/pucminas/domain/SimilarityMatrixCell.java
@@ -1,11 +1,11 @@
 package br.com.pucminas.domain;
 
-public class Similarity {
+public class SimilarityMatrixCell {
 	private Method lineMethod;
 	private Method columnMethod;
 	private double similarity;
 
-	public Similarity(Method lineMethod, Method columnMethod, double similarity) {
+	public SimilarityMatrixCell(Method lineMethod, Method columnMethod, double similarity) {
 		this.lineMethod = lineMethod;
 		this.columnMethod = columnMethod;
 		this.similarity = similarity;
